a51b5fdf92c3fc1dd35b3e967fd3303a518fcb66
Samuel Akintunde <samakintunde37@gmail.com>
2017-10-02 23:27:26 +0100

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    var swatch = [
+        '#FFAAAA',
+        '#550000',
+        '#9974AA',
+        '#280339',
+        '#FFDEAA',
+        '#553400',
+        '#827FB2',
+        '#0B083B',
+        '#9ED78F',
+        '#0F4800',
+        '#748BA7',
+        '#FFAEAC',
+        '#4D0200',
+        '#FF4900',
+        '#040100',
+        '#0E54B4',
+        '#B6D4FE',
+        '#E5004D',
+        '#FFAFCA',
+        '#CCF8E6',
+        '#004E2F',
+        '#FFCCB6',
+        '#531E07',
+        '#D7A9B4',
+        '#42111D'
+    ];
+
+    $('#get-quote').on('click', function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "GET",
+            url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+            success: function(data) {
+                var post = data.shift();
+                $('#author').text("-" + post.title);
+                $('#content').html('"' + post.content + '"');
+            },
+            cache: false,
+        });
+    });    
+    
+    $("#tweet").on("click", function(e) {
+        e.preventDefault();
+        window.open("https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + data.content + "-" + data.title);
+    });
+});
